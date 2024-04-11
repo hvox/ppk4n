498b31fe82b9e3959e845fd04b4637a12591c423
Uladzislau Khamkou <kham@tuta.io>
2024-04-11 22:30:37 +0300

    Add stack separation for functions

--- src/bytecode/interpreter.rs
+++ src/bytecode/interpreter.rs
@@ -9,46 +9,49 @@ struct State<'a> {
 fn run(program: &Mod) -> Vec<Value> {
 	let main_id = program.exports["main"];
 	let mut state = State::new(program);
-	call_fn(&mut state, main_id);
+	call_fn(program, &mut state.stack, main_id);
 	state.stack
 }
 
-fn call_fn(state: &mut State, fn_id: usize) {
-	let f = &state.code.functions[fn_id];
+fn call_fn(program: &Mod, stack: &mut Vec<Value>, fn_id: usize) {
+	let f = &program.functions[fn_id];
 	let mut locals = vec![Value::NONE; f.params.len() + f.locals.len()];
 	for i in (0..f.params.len()).rev() {
-		locals[i] = state.stack.pop().unwrap();
+		locals[i] = stack.pop().unwrap();
 	}
-	eval_block(state, &mut locals, &f.body);
+	let mut fn_stack = vec![];
+	eval_block(program, &mut fn_stack, &mut locals, &f.body);
+	let result = fn_stack.split_off(fn_stack.len() - f.result.len());
+	stack.extend(result);
 }
 
-fn eval_block(state: &mut State, locals: &mut Vec<Value>, block: &Vec<Op>) {
+fn eval_block(program: &Mod, stack: &mut Vec<Value>, locals: &mut Vec<Value>, block: &Vec<Op>) {
 	for instruction in block {
 		match instruction {
-			Op::Drop => state.stack.truncate(state.stack.len() - 1),
-			Op::Call(i) => call_fn(state, *i),
-			Op::Set(i) => locals[*i] = state.stack.pop().unwrap(),
-			Op::Tee(i) => locals[*i] = *state.stack.last().unwrap(),
-			Op::Get(i) => state.stack.push(locals[*i]),
-			Op::Const(value, _) => state.stack.push(*value),
+			Op::Drop => stack.truncate(stack.len() - 1),
+			Op::Call(i) => call_fn(program, stack, *i),
+			Op::Set(i) => locals[*i] = stack.pop().unwrap(),
+			Op::Tee(i) => locals[*i] = *stack.last().unwrap(),
+			Op::Get(i) => stack.push(locals[*i]),
+			Op::Const(value, _) => stack.push(*value),
 			Op::IfElse(then, otherwise) => {
-				let condition: i32 = state.stack.pop().unwrap().into();
+				let condition: i32 = stack.pop().unwrap().into();
 				if condition != 0 {
-					eval_block(state, locals, then);
+					eval_block(program, stack, locals, then);
 				} else {
-					eval_block(state, locals, otherwise);
+					eval_block(program, stack, locals, otherwise);
 				}
 			}
 			Op::Binary(op, typ) => match typ {
 				Type::I32 => {
-					let y: i32 = state.stack.pop().unwrap().into();
-					let x: i32 = state.stack.pop().unwrap().into();
+					let y: i32 = stack.pop().unwrap().into();
+					let x: i32 = stack.pop().unwrap().into();
 					let z = match op {
 						BinaryOp::Add => x.wrapping_add(y),
 						BinaryOp::Sub => x.wrapping_sub(y),
 						BinaryOp::Mul => x.wrapping_mul(y),
 					};
-					state.stack.push(z.into());
+					stack.push(z.into());
 				}
 			},
 		}
