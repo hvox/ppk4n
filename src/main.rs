f84354bdc77a9c5dabe46921e218d65dcb615cef
Uladzislau Khamkou <kham@tuta.io>
2024-04-12 12:46:25 +0300

    Update mains to include simple checks

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[allow(dead_code)]
 mod bytecode;
 use std::collections::HashMap;
 
@@ -14,15 +15,49 @@ fn main() {
 	let program = Program {
 		imports: IndexMap::new(),
 		exports: HashMap::from([("main".to_string(), 0)]),
-		functions: IndexMap::from([(
-			"main".to_string(),
-			Fn {
-				params: vec![],
-				result: vec![],
-				locals: vec![],
-				body: vec![Op::i32(123), Op::i32(321), Op::Binary(Add, I32)],
-			},
-		)]),
+		functions: IndexMap::from([
+			(
+				"main".to_string(),
+				Fn {
+					params: vec![],
+					result: vec![I32],
+					locals: vec![Var { id: "x".to_string(), typ: I32 }],
+					body: vec![
+						Op::i32(100),
+						Op::i32(1000),
+						Op::Drop,
+						Op::Call(1),
+						Op::i32(123456789),
+						Op::IfElse(
+							vec![
+								Op::i32(100),
+								Op::i32(42),
+								Op::Binary(Add, I32),
+								Op::i32(100),
+								Op::Binary(Sub, I32),
+								Op::Tee(0),
+							],
+							vec![Op::i32(666)],
+						),
+					],
+				},
+			),
+			(
+				"f".to_string(),
+				Fn {
+					params: vec![],
+					result: vec![I32],
+					locals: vec![],
+					body: vec![
+						Op::i32(42),
+						Op::i32(1),
+						Op::Binary(Mul, I32),
+						Op::Return,
+						Op::i32(987654321),
+					],
+				},
+			),
+		]),
 	};
 	let result = program.run();
 	println!("Program returned: {:?}", result);
--- wasm-edition/src/main.rs
+++ wasm-edition/src/main.rs
@@ -1,3 +1,20 @@
+#![allow(dead_code)]
+#![allow(unused_mut)]
+#![allow(unused_variables)]
+mod wasm;
+use std::io::IsTerminal;
+use wasm::Wasm;
+
 fn main() {
-	println!("Why do I exist");
+	match Wasm::from(include_bytes!("minimal.wasm")) {
+		Ok(_) => (),
+		Err(cause) => {
+			let prefix = "Failed to parse WASM file because of";
+			if std::io::stdout().is_terminal() {
+				eprintln!("\x1b[91m{} \x1b[93mwrong {}\x1b[91m.\x1b[0m", prefix, cause);
+			} else {
+				eprintln!("{} {}.", prefix, cause);
+			}
+		}
+	}
 }
