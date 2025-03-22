33861fc84f6839ba5a0333407adafe03a5398ba9
Uladzislau Khamkou <kham@tuta.io>
2025-03-22 14:34:15 +0300

    Remove tests with old-syntax

--- tests/functions.py
+++
@@ -1,9 +0,0 @@
-def f(x: int, y: int) -> None:
-    print(x, end="")
-    print(y)
-
-def main() -> None:
-    f(123, 456)
-
-if __name__ == "__main__":
-    main()
--- tests/shadowing.py
+++
@@ -1,9 +0,0 @@
-def main() -> None:
-    x: int = 123
-    y: int = 321
-    print(x + y)
-    x: int = x + y
-    print(x - y)
-
-if __name__ == "__main__":
-    main()
