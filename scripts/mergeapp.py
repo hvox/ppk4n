abe169b2edfc5cfad3f08c12b1b5ac95c24fb2a8
Uladzislau Khamkou <kham@tuta.io>
2025-03-23 22:18:57 +0300

    Update script mergeapp.py

--- scripts/mergeapp.py
+++ scripts/mergeapp.py
@@ -1,11 +1,13 @@
+#!/usr/bin/env python3
 from pathlib import Path
 
 INDENTATION = 2 * " "
 MOD_INDENTATION = "  "
 ROOT = Path(__file__).resolve().parent.parent / "src"
+QUEUE = [(ROOT, "main")]
 
 
-def merge_module(receiver: Path, module: str) -> list[str]:
+def process_module(receiver: Path, module: str) -> list[str]:
     path = (receiver.parent if receiver.stem == "main" else receiver.with_suffix("")) / f"{module}.rs"
     lines = []
     for line in path.read_text().splitlines():
@@ -14,16 +16,18 @@ def merge_module(receiver: Path, module: str) -> list[str]:
             continue
         elif (code.startswith("mod ") or code.startswith("pub mod ")) and code.endswith(";"):
             child_module = code.removeprefix("pub ").removeprefix("mod ")[:-1]
-            lines.append(f"pub mod {child_module} " "{")
-            for line in merge_module(path, child_module):
-                lines.append(MOD_INDENTATION + line)
-            lines.append("}")
-            continue
+            child = (path, child_module)
+            if child not in QUEUE:
+                QUEUE.append(child)
         indent = line[: len(line) - len(code)].replace("    ", INDENTATION).replace("\t", INDENTATION)
         line = indent + code
-        lines.append(line)
+        if line.strip():
+            lines.append(line)
     return lines
 
 
-for line in merge_module(ROOT, "main"):
-    print(line)
+for (path, module) in QUEUE:
+    module_path = (path.parent if path.stem == "main" else path.with_suffix("")) / f"{module}.rs"
+    print(f"\n// Содержимое файла {module_path.relative_to(ROOT.parent)}")
+    for line in process_module(path, module):
+        print(line)
